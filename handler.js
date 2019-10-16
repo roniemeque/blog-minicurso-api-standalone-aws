7f8655549993650ea4512a683ed1118f450af949
Ronie <roniemeque@icloud.com>
2019-10-16 14:51:47 -0300

    port da api para serverless framework

---
+++ handler.js
@@ -0,0 +1,93 @@
+import { getCollection } from "./lib/db";
+import { sluggify } from "./helpers/strings";
+import { success, fail } from "./lib/responses";
+
+// pega todos os posts do user
+export const busca_posts = async ({ pathParameters }) => {
+  const { user_id: userId } = pathParameters || {};
+  if (!userId) {
+    return fail({ error: "falta user_id. Ex: /api/fulano/criar" });
+  }
+
+  try {
+    const { collection, dbComCache } = await getCollection("posts");
+
+    const posts = await collection
+      .find({ userId })
+      .sort({ date: -1 })
+      .toArray();
+
+    return success({
+      posts,
+      dbComCache,
+      userId
+    });
+  } catch (error) {
+    return fail({ error, dbComCache, userId });
+  }
+};
+
+// pega um post especifico do user com post_path
+export const busca_post = async ({ pathParameters }) => {
+  const { user_id: userId, post_path: postPath } = pathParameters || {};
+  if (!userId) {
+    return fail({ error: "falta user_id. Ex: /api/fulano/criar" });
+  }
+
+  try {
+    const { collection, dbComCache } = await getCollection("posts");
+
+    const [post] = await collection.find({ path: postPath }).toArray();
+
+    return success({
+      post,
+      dbComCache,
+      userId,
+      postPath
+    });
+  } catch (error) {
+    return fail({ error, dbComCache, userId, postPath });
+  }
+};
+
+// cria um novo post
+export const cria_post = async ({ pathParameters, body }) => {
+  const { user_id: userId } = pathParameters || {};
+  if (!userId) {
+    return fail({ error: "falta user_id. Ex: /api/fulano/criar" });
+  }
+
+  const parsedBody = JSON.parse(body);
+
+  try {
+    const { collection, dbComCache } = await getCollection("posts");
+
+    // separando tags
+    const tags = parsedBody.tags
+      .split(",")
+      .map(tag => tag.trim().toLowerCase());
+
+    // criando slug do path
+    const path = `${sluggify(parsedBody.titulo)}-${Math.floor(
+      Math.random() * 999
+    )}`;
+
+    const { insertedId } = await collection.insertOne({
+      ...parsedBody,
+      userId,
+      path,
+      tags,
+      date: new Date()
+    });
+
+    return success({
+      insertedId,
+      path,
+      dbComCache,
+      userId,
+      parsedBody
+    });
+  } catch (error) {
+    return fail({ error, dbComCache, userId, parsedBody });
+  }
+};
